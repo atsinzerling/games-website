75d77a0fdd5a285db43a77af2ddb127a6ac29b4d
ArseniyZinzerling <ars.tsin@gmail.com>
2022-02-15 18:18:39 -0500

    creating navigation bar

---
+++ games-web/src/Games.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Games.css';
+
+function Games (){
+  
+  return (
+    <div class = "games">
+               <li class = "dot"><a href="/smth">Game 1</a></li>
+               <li class = "dot"><a href="/smth">Game 2</a></li>
+               <li class = "dot"><a href="/smth">Game 3</a></li>
+               <li class = "dot"><a href="/smth">Game 4</a></li>
+         </div>
+  );
+  }
+
+export default Games;
---
+++ games-web/src/NavBar.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function NavBar() {
+  return (
+    <div class = "showcase">
+               <li><a href="index.html">Menu</a></li>
+               <li><a href='/games'>Games</a></li>
+               <li><a href="trending.html">Trending</a></li>
+               <li><a href="aboutus.html">About us</a></li>
+         </div>
+  );
+}
+
+export default NavBar;
